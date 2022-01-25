474e450872cded4bc805603eaf5d6e359b18789e
José Manuel Vera <josemanuel.vera.aray@gmail.com>
2022-01-25 14:45:06 +0000

    Add footer component

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<footer className="bg-dark text-light">
+			<div className="row p-3 justify-content-center">
+				Copyright @ Your Website 2022
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
