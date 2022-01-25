3cbfd6c0eafb195265b8e1c15c0fbcc538a1f295
José Manuel Vera <josemanuel.vera.aray@gmail.com>
2022-01-25 14:44:37 +0000

    Add jumbotron component

---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const JumboTron = () => {
+	return (
+		<div className="bg-light p-5">
+			<div className="display-5 fw-light">A Warm Welcome!</div>
+			<div className="lead">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+				ac erat ac nisi pharetra lobortis eget sed justo. Vestibulum
+				consequat, quam ut iaculis efficitur, nunc quam consequat leo,
+				et ultrices purus odio non nisl. Aenean risus velit, cursus sed
+				velit eget, dignissim ultricies lorem. Praesent id sem semper,
+				dapibus odio ullamcorper, interdum nunc. Etiam ac tincidunt
+				arcu. Donec pulvinar quam nunc, at viverra lacus faucibus ac
+			</div>
+		</div>
+	);
+};
+export default JumboTron;
