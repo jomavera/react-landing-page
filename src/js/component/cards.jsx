d1d672706aaff0cf283c034fe8c2f7ae69d56d48
José Manuel Vera <josemanuel.vera.aray@gmail.com>
2022-01-26 14:45:00 +0000

    Add cards component

---
+++ src/js/component/cards.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+
+const Cards = () => {
+	return (
+		<div className="row">
+			<div className="col">
+				<div className="card m-3">
+					<img
+						src="https://via.placeholder.com/500x325"
+						class="card-img-top"
+						alt="photo"
+					/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit. Donec ac erat ac nisi pharetra lobortis eget
+							sed justo.
+						</p>
+						<a href="#" className="btn btn-primary">
+							Find Out Now!
+						</a>
+					</div>
+				</div>
+			</div>
+			<div className="col">
+				<div className="card m-3">
+					<img
+						src="https://via.placeholder.com/500x325"
+						class="card-img-top"
+						alt="photo"
+					/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit. Donec ac erat ac nisi pharetra lobortis eget
+							sed justo.
+						</p>
+						<a href="#" className="btn btn-primary">
+							Find Out Now!
+						</a>
+					</div>
+				</div>
+			</div>
+			<div className="col">
+				<div className="card m-3">
+					<img
+						src="https://via.placeholder.com/500x325"
+						class="card-img-top"
+						alt="photo"
+					/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit. Donec ac erat ac nisi pharetra lobortis eget
+							sed justo.
+						</p>
+						<a href="#" className="btn btn-primary">
+							Find Out Now!
+						</a>
+					</div>
+				</div>
+			</div>
+			<div className="col">
+				<div className="card m-3">
+					<img
+						src="https://via.placeholder.com/500x325"
+						class="card-img-top"
+						alt="photo"
+					/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit. Donec ac erat ac nisi pharetra lobortis eget
+							sed justo.
+						</p>
+						<a href="#" className="btn btn-primary">
+							Find Out Now!
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Cards;
