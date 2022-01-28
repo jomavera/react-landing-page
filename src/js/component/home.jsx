01a4f5fc32d63430f52d94bc5196e3634dd22ee1
José Manuel Vera <josemanuel.vera.aray@gmail.com>
2022-01-28 14:16:18 +0000

    Update home.jsx to include all components

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import NavBar from "./navbar.jsx";
+import JumboTron from "./jumbotron.jsx";
+import Cards from "./cards.jsx";
+import Footer from "./footer.jsx";
 
 //create your first component
 const Home = () => {
 	return (
-		<div>
+		<div className="container-fluid">
 			<NavBar />
+			<JumboTron />
+			<Cards />
+			<Footer />
 		</div>
 	);
 };
