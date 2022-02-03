e5791498367899f9e5b7c40a2ce6b12936639019
José Manuel Vera <josemanuel.vera.aray@gmail.com>
2022-02-03 12:44:53 +0000

    Add conditional rendering on cards component

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -1,13 +1,167 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 const Cards = () => {
-	return (
-		<div className="row">
-			<div className="col">
+	const [anchoPantalla, setAnchoPatalla] = useState(1000);
+
+	const handleResize = () => {
+		setAnchoPatalla(
+			Math.max(
+				document.documentElement.clientWidth || 0,
+				window.innerWidth || 0
+			)
+		);
+	};
+	useEffect(() => {
+		window.addEventListener("resize", handleResize);
+	}, []);
+	console.log(anchoPantalla);
+	if (anchoPantalla > 820) {
+		return (
+			<div className="row">
+				<div className="col">
+					<div className="card m-3">
+						<img
+							src="https://via.placeholder.com/500x325"
+							className="card-img-top"
+							alt="photo"
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">
+								Lorem ipsum dolor sit amet, consectetur
+								adipiscing elit. Donec ac erat ac nisi pharetra
+								lobortis eget sed justo.
+							</p>
+							<a href="#" className="btn btn-primary">
+								Find Out Now!
+							</a>
+						</div>
+					</div>
+				</div>
+				<div className="col">
+					<div className="card m-3">
+						<img
+							src="https://via.placeholder.com/500x325"
+							className="card-img-top"
+							alt="photo"
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">
+								Lorem ipsum dolor sit amet, consectetur
+								adipiscing elit. Donec ac erat ac nisi pharetra
+								lobortis eget sed justo.
+							</p>
+							<a href="#" className="btn btn-primary">
+								Find Out Now!
+							</a>
+						</div>
+					</div>
+				</div>
+				<div className="col">
+					<div className="card m-3">
+						<img
+							src="https://via.placeholder.com/500x325"
+							className="card-img-top"
+							alt="photo"
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">
+								Lorem ipsum dolor sit amet, consectetur
+								adipiscing elit. Donec ac erat ac nisi pharetra
+								lobortis eget sed justo.
+							</p>
+							<a href="#" className="btn btn-primary">
+								Find Out Now!
+							</a>
+						</div>
+					</div>
+				</div>
+				<div className="col">
+					<div className="card m-3">
+						<img
+							src="https://via.placeholder.com/500x325"
+							className="card-img-top"
+							alt="photo"
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">
+								Lorem ipsum dolor sit amet, consectetur
+								adipiscing elit. Donec ac erat ac nisi pharetra
+								lobortis eget sed justo.
+							</p>
+							<a href="#" className="btn btn-primary">
+								Find Out Now!
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	} else {
+		return (
+			<div className="vstack">
 				<div className="card m-3">
 					<img
 						src="https://via.placeholder.com/500x325"
-						class="card-img-top"
+						className="card-img-top"
+						alt="photo"
+					/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit. Donec ac erat ac nisi pharetra lobortis eget
+							sed justo.
+						</p>
+						<a href="#" className="btn btn-primary">
+							Find Out Now!
+						</a>
+					</div>
+				</div>
+				<div className="card m-3">
+					<img
+						src="https://via.placeholder.com/500x325"
+						className="card-img-top"
+						alt="photo"
+					/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit. Donec ac erat ac nisi pharetra lobortis eget
+							sed justo.
+						</p>
+						<a href="#" className="btn btn-primary">
+							Find Out Now!
+						</a>
+					</div>
+				</div>
+				<div className="card m-3">
+					<img
+						src="https://via.placeholder.com/500x325"
+						className="card-img-top"
+						alt="photo"
+					/>
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit. Donec ac erat ac nisi pharetra lobortis eget
+							sed justo.
+						</p>
+						<a href="#" className="btn btn-primary">
+							Find Out Now!
+						</a>
+					</div>
+				</div>
+				<div className="card m-3">
+					<img
+						src="https://via.placeholder.com/500x325"
+						className="card-img-top"
 						alt="photo"
 					/>
 					<div className="card-body">
@@ -23,68 +177,8 @@ const Cards = () => {
 					</div>
 				</div>
 			</div>
-			<div className="col">
-				<div className="card m-3">
-					<img
-						src="https://via.placeholder.com/500x325"
-						class="card-img-top"
-						alt="photo"
-					/>
-					<div className="card-body">
-						<h5 className="card-title">Card title</h5>
-						<p className="card-text">
-							Lorem ipsum dolor sit amet, consectetur adipiscing
-							elit. Donec ac erat ac nisi pharetra lobortis eget
-							sed justo.
-						</p>
-						<a href="#" className="btn btn-primary">
-							Find Out Now!
-						</a>
-					</div>
-				</div>
-			</div>
-			<div className="col">
-				<div className="card m-3">
-					<img
-						src="https://via.placeholder.com/500x325"
-						class="card-img-top"
-						alt="photo"
-					/>
-					<div className="card-body">
-						<h5 className="card-title">Card title</h5>
-						<p className="card-text">
-							Lorem ipsum dolor sit amet, consectetur adipiscing
-							elit. Donec ac erat ac nisi pharetra lobortis eget
-							sed justo.
-						</p>
-						<a href="#" className="btn btn-primary">
-							Find Out Now!
-						</a>
-					</div>
-				</div>
-			</div>
-			<div className="col">
-				<div className="card m-3">
-					<img
-						src="https://via.placeholder.com/500x325"
-						class="card-img-top"
-						alt="photo"
-					/>
-					<div className="card-body">
-						<h5 className="card-title">Card title</h5>
-						<p className="card-text">
-							Lorem ipsum dolor sit amet, consectetur adipiscing
-							elit. Donec ac erat ac nisi pharetra lobortis eget
-							sed justo.
-						</p>
-						<a href="#" className="btn btn-primary">
-							Find Out Now!
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+		);
+	}
 };
 
 export default Cards;
